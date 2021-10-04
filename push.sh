b2587edfd9467e1035a70c9e89c240e94e8cfbbf
Ross Edlin <rossedlin@gmail.com>
2021-10-04 10:42:05 +0700

    add gcloud

--- push.sh
+++ push.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env bash
 
+# Push to Docker Hub
 docker push rossedlin/php-apache:7.4
+
+# Tag the image
+#docker tag rossedlin/php-apache:7.4 asia-southeast1-docker.pkg.dev/cryslo-test/minecraft/php-apache:7.4
+
+#Push to GCloud
+#docker push asia-southeast1-docker.pkg.dev/cryslo-test/minecraft/php-apache:7.4
