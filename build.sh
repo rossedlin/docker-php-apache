7365de65d978c31515a646df44d669c6d950299e
Ross Edlin <rossedlin@gmail.com>
2024-06-28 13:14:15 +0100

    Update build scripts

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
+docker compose down
 docker build -t rossedlin/php-apache:7.4 .
+docker build -t ghcr.io/rossedlin/php-apache:7.4 .
--- push.sh
+++ push.sh
@@ -3,8 +3,5 @@
 # Push to Docker Hub
 docker push rossedlin/php-apache:7.4
 
-# Tag the image
-#docker tag rossedlin/php-apache:7.4 asia-southeast1-docker.pkg.dev/cryslo-test/minecraft/php-apache:7.4
-
-#Push to GCloud
-#docker push asia-southeast1-docker.pkg.dev/cryslo-test/minecraft/php-apache:7.4
+#Push to GitHub
+#docker push ghcr.io/rossedlin/php-apache:7.4
