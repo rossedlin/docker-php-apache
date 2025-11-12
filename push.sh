3700b38be5a2667a1c5642fb30727dbca0d44c7d
Ross Edlin <rossedlin@gmail.com>
2025-11-12 13:43:32 +0000

    Make PHP v8.4

--- push.sh
+++ push.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 # Push to Docker Hub
-docker push rossedlin/php-apache:8.2
+docker push rossedlin/php-apache:8.4
 
 # Tag the image
-#docker tag rossedlin/php-apache:8.2 asia-southeast1-docker.pkg.dev/cryslo-test/minecraft/php-apache:8.2
+#docker tag rossedlin/php-apache:8.4 asia-southeast1-docker.pkg.dev/cryslo-test/minecraft/php-apache:8.4
 
 #Push to GCloud
-#docker push asia-southeast1-docker.pkg.dev/cryslo-test/minecraft/php-apache:8.2
+#docker push asia-southeast1-docker.pkg.dev/cryslo-test/minecraft/php-apache:8.4
